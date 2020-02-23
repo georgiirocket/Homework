596cbe243d5283a7afb83959fbb662a272ec795c
georgiirocket <60443511+georgiirocket@users.noreply.github.com>
2020-02-23 22:13:03 +0300

    homework 23.02.2020
    add fonts 
    add jquery

--- maket/js/js.js
+++ maket/js/js.js
@@ -6,3 +6,30 @@ menu.onclick = function myFunction() {
         x.className = 'nav';
     }
 }
+
+$('.input-informationName').attr('placeholder', 'Name').addClass('placeholder-style');
+$('.input-informationEmail').attr('placeholder', 'Email').addClass('placeholder-style');
+$('.input-informationSubject').attr('placeholder', 'Subject').addClass('placeholder-style');
+$('.input-informationMessage').attr('placeholder', 'Message').addClass('placeholder-style');
+
+
+
+$(function () {
+    $("#tabsOne").on("click", function () {
+        $(".town-text-block").removeClass("tab-active");
+        $('#tabTextContentOne').addClass("tab-active");
+    });
+    $("#tabsOne").trigger('click');
+});
+
+
+
+$("#tabsTwo").on("click", function () {
+    $(".town-text-block").removeClass("tab-active");
+    $('#tabTextContentTwo').addClass("tab-active");
+});
+
+$("#tabsThree").on("click", function () {
+    $(".town-text-block").removeClass("tab-active");
+    $('#tabTextContentThree').addClass("tab-active");
+});
